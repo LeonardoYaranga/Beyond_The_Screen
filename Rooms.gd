5ad561344a1aba85bca39196a44d125fb59c967c
Leonardo <leonardo.yaranga@gmail.com>
2025-06-15 18:28:29 -0500

    ADD: Navigation between rooms of world 1

---
+++ Rooms.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+const ROOM_SCENES: Dictionary = {
+	"Room1_1": preload("res://Rooms/Room1_1.tscn"),  # Prision (start)
+	"Room1_2": preload("res://Rooms/Room1_2.tscn"),  # Town (hub)
+	"Room1_3": preload("res://Rooms/Room1_3.tscn"),  # Forja
+	"Room1_4": preload("res://Rooms/Room1_4.tscn"),  # Taberna
+	"Room1_5": preload("res://Rooms/Room1_5.tscn")  # Cueva
+}
+
+@onready var player: CharacterBody2D = get_parent().get_node("Player")
+var current_room: Node2D = null
+var current_room_name: String = ""
+
+func _ready() -> void:
+	# Instanciar la sala inicial (cárcel)
+	_load_room("Room1_1")
+
+func _load_room(room_name: String) -> void:
+	if current_room:
+		if current_room.has_signal("door_entered"):
+			current_room.door_entered.disconnect(_on_door_entered)
+		current_room.queue_free()
+	
+	current_room = ROOM_SCENES[room_name].instantiate()
+	current_room_name = room_name
+	call_deferred("_add_room_deferred")
+	
+func _add_room_deferred() -> void:
+	add_child(current_room)
+	
+	var spawn_pos = current_room.get_node("PlayerSpawnPos").position
+	player.position = spawn_pos
+	
+	if current_room.has_signal("door_entered"):
+		print("Conectando door_entered para sala:", current_room_name)
+		if not current_room.door_entered.is_connected(_on_door_entered):
+			current_room.door_entered.connect(_on_door_entered)
+			
+func _on_door_entered(door: Node2D) -> void:
+	# Determinar a qué sala lleva la puerta
+	print("Rooms: Cambiando a sala:", door.target_room)
+	var target_room = door.target_room
+	if target_room:
+		_load_room(target_room)
--- Rooms/Door.gd
+++ Rooms/Door.gd
@@ -1,6 +1,29 @@
 extends StaticBody2D
 
+signal door_entered(door: Node2D)
+
+@export var target_room: String = ""  # Sala destino (por ejemplo, "room1_5")
+var is_open: bool = false
+
 @onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
+@onready var collision_shape: CollisionShape2D = get_node("CollisionShape2D")
+@onready var detector: Area2D = get_node("Detector")
+
+func _ready() -> void:
+	if detector:
+		detector.body_entered.connect(_on_detector_body_entered)
+	else:
+		printerr("Error: Nodo 'Detector' (Area2D) no encontrado en Door")
+	if is_open:
+		collision_shape.disabled = true
 
 func open() -> void:
-	animation_player.play("open")
+	if not is_open:
+		is_open = true
+		animation_player.play("open")
+
+func _on_detector_body_entered(body: Node2D) -> void:
+	print("Cuerpo en puerta detectado")
+	if body.is_in_group("player") and is_open:
+		print("Door: Player cruzó la puerta, destino:", target_room)
+		door_entered.emit(self)
