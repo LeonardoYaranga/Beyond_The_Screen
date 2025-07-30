776dd79a3442b495bab3a75fe89877ef5e27e1dc
Rabedon1 <rabedon1@espe.edu.ec>
2025-07-29 20:16:05 -0500

    modificacion del skeletonSeeker

---
+++ Characters/Enemy/Skeleton_Seeker/skeletonSeekerFSM.gd
@@ -0,0 +1,35 @@
+extends FiniteStateMachine
+
+func _init() -> void:
+	_add_state("walk")
+	#_add_state("hurt")
+	#_add_state("dead")
+	
+	
+func _ready() -> void:
+	set_state(states.walk)
+	
+	
+func _state_logic(_delta: float) -> void:
+	#print("Estado actual flyingCreatureFSM:", state)
+	if state == states.walk:
+		parent.chase()
+		parent.move()
+		
+		
+#func _get_transition() -> int:
+#	match state:
+#		states.hurt:
+#			if not animation_player.is_playing():
+#				return states.chase
+#	return -1
+	
+	
+func _enter_state(_previous_state: int, new_state: int) -> void:
+	match new_state:
+		states.walk:
+			animation_player.play("walk")
+		#states.hurt:
+		#	animation_player.play("hurt")
+	#	states.dead:
+	#		animation_player.play("dead")
